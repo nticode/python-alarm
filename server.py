12dcc5985c492d64876239eb21cca43fd3e47b71
Kevie <65157770+Kevsay@users.noreply.github.com>
2023-01-16 13:58:10 +0100

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,12 @@ from flask import Flask, request
 app = Flask(__name__)
 
 import datetime
-import os
+
+global odistance
+global change
+
+odistance = None
+change = 25
 
 @app.route('/api/create', methods=["POST"])
 def create():
@@ -10,12 +15,34 @@ def create():
     timestamp = request.args.get("timestamp")
     distance = request.args.get("distance")
 
-    f = open(f"./logs/{time}.txt", "a")
+    f = open(f"./logs/{time}_create.txt", "a")
     f.write(f"[{timestamp}] {distance}\n")
     f.close()
 
     print(f"[{timestamp}] {distance}\n")
     
+    return "True"
+
+@app.route('/api/changed', methods=["POST"])
+def changed():
+    global odistance
+    global change
+    global old
+    time = request.args.get("time")
+    timestamp = request.args.get("timestamp")
+    distance = request.args.get("distance")
+    rdistance = int(distance.split(".")[0])
+
+    if not odistance is None:
+
+        if odistance - change >= rdistance <= odistance + change:
+
+            f = open(f"./logs/{time}_changed.txt", "a")
+            f.write(f"[{timestamp}] from: {old} to: {distance}\n")
+            f.close()
+    
+    odistance = rdistance
+    old = distance
     return "True"
 
 app.run(host='192.168.0.9', port=5000)
