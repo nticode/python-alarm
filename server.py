aa0039b403924c7d212a72cc969ab2bc9c64f8ba
Kevie <65157770+Kevsay@users.noreply.github.com>
2023-01-16 13:06:21 +0100

    Update server.py

--- server.py
+++ server.py
@@ -8,14 +8,14 @@ import os
 def create():
     time = request.args.get("time")
     timestamp = request.args.get("timestamp")
-    distence = request.args.get("distence")
+    distance = request.args.get("distance")
 
     f = open(f"./logs/{time}.txt", "a")
-    f.write(f"[{timestamp}] {distence}\n")
+    f.write(f"[{timestamp}] {distance}\n")
     f.close()
 
-    print(f"[{timestamp}] {distence}\n")
+    print(f"[{timestamp}] {distance}\n")
     
     return "True"
 
-app.run(host='ip', port=5000)
+app.run(host='192.168.0.9', port=5000)
